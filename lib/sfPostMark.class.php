62d9b674f6d8ac1081732a37c7a4cc9ef368934e
Tomasz Zdanowski <tomasz@amilo.(none)>
2011-01-05 22:24:09 +0100

    first commit

---
+++ lib/sfPostMark.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Postmark PHP class
+ * 
+ * Copyright 2010, Markus Hedlund, Mimmin AB, www.mimmin.com
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author Markus Hedlund (markus@mimmin.com) at mimmin (www.mimmin.com)
+ * @copyright Copyright 2009 - 2010, Markus Hedlund, Mimmin AB, www.mimmin.com
+ * @version 0.4.4
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * 
+ * Usage:
+ * Mail_Postmark::compose()
+ *      ->addTo('address@example.com', 'Name')
+ *      ->subject('Subject')
+ *      ->messagePlain('Plaintext message')
+ *	    ->tag('Test tag')
+ *      ->send();
+ * 
+ * or:
+ * 
+ * $email = new Mail_Postmark();
+ * $email->addTo('address@example.com', 'Name')
+ *      ->subject('Subject')
+ *      ->messagePlain('Plaintext message')
+ *	    ->tag('Test tag')
+ *      ->send();
+ */
+ 
+class sfPostMark extends Mail_Postmark
+{
+  public function __construct()
+  {
+    $this->_apiKey = sfPostMarkAdapter::getApiKey();      
+    sfPostMarkAdapter::setupDefaults($this);
+    $this->messageHtml(null)->messagePlain(null);
+  }
+
+  public static function compose()
+  {
+    return new self();
+  }
+
+  protected function _log($logData)
+  {
+    sfPostMarkAdapter::log($logData);
+  }
+}
