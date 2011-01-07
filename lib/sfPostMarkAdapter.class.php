50431050af70424b630282ba7752b4cecec3ddbd
Tomasz Zdanowski <tomasz@amilo.(none)>
2011-01-07 14:22:01 +0100

    body moved to plugin class

--- lib/sfPostMarkAdapter.class.php
+++ lib/sfPostMarkAdapter.class.php
@@ -1,23 +1,5 @@
 <?php
 
-class sfPostMarkAdapter implements Mail_Postmark_Adapter_Interface
+class sfPostMarkAdapter extends PluginsfPostMarkAdapter
 {
-  public static $latestLogEntry;
-  
-  public static function getApiKey()
-  {
-    return sfConfig::get('app_sf_postmark_plugin_key',Mail_Postmark::TESTING_API_KEY);
-  }
-  
-  public static function setupDefaults(Mail_Postmark &$mail)
-  {
-    $from = sfConfig::get('app_sf_postmark_plugin_from','foo@bar.com');
-    $name = sfConfig::get('app_sf_postmark_plugin_name','Foo Bar');
-    $mail->from($from, $name);
-  }
-  
-  public static function log($logData)
-  {
-    self::$latestLogEntry = $logData;
-  }
 }
